8202427110da10068d1f8e5f5d62598638cc8421
Laura Trabas Clavero <Laura@MacBook-Pro-de-Laura-2.local>
2017-04-15 15:15:08 +0200

    Ejercicio

--- myproject/urls.py
+++ myproject/urls.py
@@ -6,5 +6,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'myproject.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^cms/$', 'cms.views.mostrar'),
+    url(r'^cms/(\d+)', 'cms.views.pagina'),
     url(r'^admin/', include(admin.site.urls)),
 )
